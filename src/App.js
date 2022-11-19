23ee8761268af1c70d44a5a1f1e4f43831a778d2
Muneeb Muhammad <muneebkattak57@gmail.com>
2022-11-19 18:05:58 +0500

    names

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
   return (
     <div>
       <form>
-        <label htmlFor='first'>Youtube Finals Your First name from tests</label>
+        <label htmlFor='first'>muneeb First name</label>
         <input type="text" id ="first" placeholder='first name' onChange={(e)=> setFirst(e.target.value)}></input>
-        <label htmlFor='last'>Yourtube Finals Your last name  from tests</label>
+        <label htmlFor='last'>muneeb Last name</label>
         <input type="text" id ="last" placeholder='last name' onChange={(e)=> setlast(e.target.value)}></input>
         <button type="button" onClick={handleSubmit}>Submit</button>
       </form>
